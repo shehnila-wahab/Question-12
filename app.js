628d304b1a69a936e636c054f857a025f64b80c2
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 22:43:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var members = ['Azam', 'Aman', 'Arman', 'Saba', 'sana'];
+var message = 'kl birthhday hai kis ki..?';
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let members: string[] =['Azam','Aman','Arman','Saba','sana'];
+let message: string='kl birthhday hai kis ki..?';
+for (let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
